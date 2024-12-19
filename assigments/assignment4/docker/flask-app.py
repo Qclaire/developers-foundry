5ee51c483507363b85d8a9892150be18cd6b0a0a
Oswald Gyabaah <oswald.gyabaah@gtbank.com>
2024-12-19 22:11:37 +0000

    flask app deployment

--- assigments/assignment4/docker/flask-app.py
+++ assigments/assignment4/docker/flask-app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Docker!"
+    welcome_message = os.getenv("WELCOME_MSG", "Hello, Docker!")
+    return welcome_message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
